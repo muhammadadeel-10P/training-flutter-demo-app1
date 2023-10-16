e44520012ba805b55c2671e90d89035283413996
Muhammad Adeel <adeel.mushtaq@10pearls.com>
2023-10-16 11:36:22 +0500

    complete

---
+++ lib/src/providers/meals_provider.dart
@@ -0,0 +1,6 @@
+import 'package:demoflutter/src/data/dummy_data.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final mealsProvider = Provider((ref) {
+  return dummyMeals;
+});
---
+++ lib/src/screens/categories.dart
@@ -0,0 +1,53 @@
+import 'package:demoflutter/src/data/dummy_data.dart';
+import 'package:demoflutter/src/models/category.dart';
+import 'package:demoflutter/src/models/meal.dart';
+import 'package:demoflutter/src/screens/meals.dart';
+import 'package:demoflutter/src/widgets/category_grid_item.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({
+    super.key,
+    required this.availableMeals,
+  });
+
+  final List<Meal> availableMeals;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    ); // Navigator.push(context, route)
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [
+        // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          )
+      ],
+    );
+  }
+}
